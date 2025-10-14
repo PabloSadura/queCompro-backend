73e8428af747a2fd40fb5900733924a43b8f31b2
Pablo Sadura <pablosadura@gmail.com>
2025-10-14 19:58:56 -0300

    cambio en el index

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import productDetail from "./routes/product.routes.js";
 dotenv.config()
 const app = express();
 
-const allowedOrigins = ["https://que-compro-frontend.onrender.com/"]; // 
+const allowedOrigins = [process.env.FRONTEND_URL]; // 
 app.use(cors({ 
   origin: allowedOrigins,   // no usar "*"
   credentials: true,        // permite enviar cookies / headers auth
